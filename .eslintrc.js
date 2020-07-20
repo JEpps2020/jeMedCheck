370d26f2bb79d2cdf806f4fad05187570226f8ad
Jasmine Epps <epps.jasmine@gmail.com>
2020-07-19 23:24:03 -0400

    je final project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	extends: 'airbnb',
+	root: true,
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+	},
+	settings: {
+    react: {
+        "version": "latest",
+    },
+},
+};
